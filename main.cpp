e6a26c1639aac4a5d0b5e6f096875d756a1a3e8e
PCristian00 <cristianp9300@gmail.com>
2022-11-07 18:17:35 +0100

    Matrice test

--- main.cpp
+++ main.cpp
@@ -9,18 +9,31 @@ int main() {
     cout << "RIDUZIONE A SCALA" << endl;
     int m[3][3];
 
-    // Inserimento matrice
+    int test[2][3]={{3,2,1},{2,-4,8}};
+
+    /* Inserimento matrice m
     for (int i=0;i<3;i++)
         for(int j=0;j<3;j++){
             cout<<"Inserire valore"<<endl;
             cin>>m[i][j];
         }
-    // Stampa matrice
+        */
+    /* Stampa matrice m
     for (int i=0;i<3;i++){
         for(int j=0;j<3;j++)
             cout<<m[i][j]<<"\t";
         cout<<endl;
     }
+     */
+
+    for (int i=0;i<2;i++){
+        for(int j=0;j<3;j++)
+            cout<<test[i][j]<<"\t";
+        cout<<endl;
+    }
+
+    // Scelta della prima colonna non nulla
+
 
     return 0;
 }
